a4b8f927f897e3b4c0d6152c6c8c268655636a6e
cpaynejohnson <94479164+cpaynejohnson@users.noreply.github.com>
2022-06-17 20:05:59 -0500

    Update index.test.js

--- backend/models/index.test.js
+++ backend/models/index.test.js
@@ -6,10 +6,15 @@ describe('School Model', () => {
 		await sequelize.sync({force: true})
 	});
 
-	test('can find a School', async() => {
-		const testSchool = await School.findOne({name : 'Spelman College'})
-		expect(testSchool.name).toBe('Spelman College')
-	})
+	// test('can find a School', async() => {
+	// 	const testSchool = await School.findOne({name : 'Spelman College'})
+	// 	expect(testSchool.name).toBe('Spelman College')
+	// })
 
+		test('School has a website', async () => {
+			const websiteStr = "https://www.spelman.edu/";
+			const testSchool = await School.create({ name: "Spelman College" , website: websiteStr});
+      expect(testSchool.website).toBe(websiteStr);
+    });
 
 })
